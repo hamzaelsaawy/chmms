4e9931f523a2a7d8f7856e23958f877d2dfc696f
Hamza El-Saawy <hamza.elsaawy@gmail.com>
2017-08-12 18:20:19 -0700

    making a more structured data training thing...

--- src/chmm.jl
+++ src/chmm.jl
@@ -4,7 +4,18 @@
 
 import StatsBase: wsample
 
-function rand_chmm(K, D)
+struct Chmm
+    K::Int # number states
+    D::Int
+
+    π0::Vector{Float64}
+    P::Array{Float64, 3}
+    μs::Vector{Vector{Float64}}
+    Σs::Vector{Matrix{Float64}}
+end
+
+# make the μ and Σ generation more sophisticated?
+function rand_chmm(K::Int=5, D::Int=3, μ_scale::Real=10, Σ_scale::Real=1)
     π0 = rand(K)
     π0 ./= sum(π0)
 
@@ -13,34 +24,78 @@ function rand_chmm(K, D)
     P = rand(K, K, K)
     P ./= sum(P, 1)
 
-    μs = [randn(D) * 8 for _ in 1:K]
-    Σs = [eye(D) * rand() for _ in 1:K]
+    μs = [randn(D) * μ_scale for _ in 1:K]
+    Σs = [eye(D) * rand() * Σ_scale for _ in 1:K]
 
-    model = Dict(:π0 => π0, :P => P,
-        :μs => μs, :Σs => Σs, :K => K, :D => D)
-
-    return model
+    return Chmm(K, D, π0, P, μs, Σs)
 end
 
-function simulate_model(model, T)
-    D = model[:D]
-    Z = Matrix{Int}(2, T)
-    X1 = Matrix{Float64}(D, T)
+function simulate_model(model::Chmm, T::Int=500)
+    D = model.D
+    Z = empty(Int, 2, T)
+    X1 = empty(D, T)
     X2 = similar(X1)
 
-    sqrtm_Σs = map(sqrtm, model[:Σs])
-    z = wsample(1:K, model[:π0], 2)
+    sqrtm_Σs = map(sqrtm, model.Σs)
+    z = wsample(1:K, model.π0, 2)
 
     for t in 1:T
-        z1 = wsample(1:K, model[:P][:, z...])
-        z2 = wsample(1:K, model[:P][:, reverse(z)...])
+        z1 = wsample(1:K, model.P[:, z...])
+        z2 = wsample(1:K, model.P[:, reverse(z)...])
         z = [z1, z2]
 
         Z[:, t] = z
-        X1[:, t] = sqrtm_Σs[z[1]] * randn(D) + model[:μs][z[1]]
-        X2[:, t] = sqrtm_Σs[z[2]] * randn(D) + model[:μs][z[2]]
+        X1[:, t] = sqrtm_Σs[z[1]] * randn(D) + model.μs[z[1]]
+        X2[:, t] = sqrtm_Σs[z[2]] * randn(D) + model.μs[z[2]]
     end
 
     return (Z, X1, X2)
+end
+
+function rand_trajs(model::Chmm; T_range::Range{Int64}=750:1_000, N_pairs::Int=5)
+    N_trajs = N_pairs * 2
+    trajs = [simulate_model(model, rand(T_range)) for _ in 1:N_pairs]
+
+    traj_lens = map(t -> size(t[1], 2), trajs)
+    num_obs = sum(traj_lens) * 2
+
+    X = empty(model.D, num_obs)
+    Z = empty(Int, num_obs)
+    traj_ptr = empty(Int, N_trajs + 1)
+    traj_ptr[1] = 1
+    traj_pairs = Vector{NTuple{2, Int}}(N_pairs)
+
+    idx = 1
+    # concatenate all the data into a single stream
+    for n in 1:N_pairs
+        (Zt, X1, X2) = trajs[n]
+        T = size(Zt, 2)
+
+        # the trajectory number
+        id1 = sub2ind((2, N_pairs), 1, n)
+        id2 = id1 + 1
+
+        # start and stop indices in global data
+        start_1 = idx
+        end_1 = start_1 + T - 1
+        start_2 = end_1 + 1
+        end_2 = start_2 + T - 1
+
+        Z[start_1:end_1] = id1 #Zt[1, :]
+        Z[start_2:end_2] = id2 #Zt[2, :]
+        X[:, start_1:end_1] = X1
+        X[:, start_2:end_2] = X2
+
+        # start of each traj
+        traj_ptr[id2] = start_2
+        # edge case for last entry
+        traj_ptr[id2+1] = end_2 + 1
+        # record the pairs
+        traj_pairs[n] = (id1, id2)
+
+        idx = end_2 + 1
+    end
+
+    return (X, Z, traj_ptr, traj_pairs)
 end
 
